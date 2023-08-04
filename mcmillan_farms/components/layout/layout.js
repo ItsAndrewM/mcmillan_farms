e960efc93c95c86e32eb55288d495ce0eeef1ae7
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-08-04 17:02:00 -0400

    added navbar and gtag

---
+++ mcmillan_farms/components/layout/layout.js
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../../styles/utils.module.css";
+import Link from "next/link";
+import Navbar from "../navbar/navbar";
+import Script from "next/script";
+
+const name = "Andrew";
+export const siteTitle = "McMillan Farms";
+
+const Layout = ({ children, home }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <div className="google-analytics-container">
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-C4HJJ32CM8"
+          ></Script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-C4HJJ32CM8');`}
+          </Script>
+        </div>
+      </Head>
+
+      <Navbar />
+      <main className={styles.main}>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/" className={styles.smallLink}>
+            ← Back to home
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Layout;
