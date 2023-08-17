b19dd78e06b47caf0748d20d24a47ad0dfe95fae
Andrew McMillan <andrewmcmillan@precisionsailloft.com>
2023-08-17 10:06:06 -0700

    added footer data

--- mcmillan_farms/components/footer/footer.js
+++ mcmillan_farms/components/footer/footer.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 import styles from "./footer.module.css";
 import utilStyles from "../../styles/utils.module.css";
 import Link from "next/link";
-import logo from "../../public/images/logo_large.png";
+import layoutStyles from "../layout/layout.module.css"
+import logo from "../../public/images/logos/logo_text_right.png";
+import { footerData } from "@/data/footerData";
 
 const Footer = () => {
   return (
@@ -19,13 +21,30 @@ const Footer = () => {
               />
             </Link>
           </div>
-          <div></div>
-          <div></div>
+          <div className={styles.container}>
+            {footerData.map((menuItem, index) => {
+              return (
+                <ul className={styles.column} key={index}>
+                  <li><h3><Link href={menuItem.link} className={layoutStyles.link}>{menuItem.name}</Link></h3></li>
+                  <ul>
+                    {menuItem.submenu.map((subMenuItem, index) => {
+                      return (
+                        <li key={index}>{subMenuItem.icon && subMenuItem.icon}<Link className={layoutStyles.link} href={subMenuItem.link}>{subMenuItem.name}</Link></li>
+                      )
+                    })}
+                  </ul>
+                </ul>
+              )
+            })}
+          </div>
         </div>
         <div className={styles.container}>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div><p>Copyright © 2023 by Ron & Colleen McMillan</p></div>
+          <div className={styles.legal}>
+            <span><Link className={layoutStyles.link} href={"/"}>Privacy Policy</Link></span>
+            <span>|</span>
+            <span><Link className={layoutStyles.link} href={"/"}>Terms and Conditions</Link></span>
+          </div>
         </div>
       </div>
     </footer>
--- mcmillan_farms/data/footerData.js
+++ mcmillan_farms/data/footerData.js
@@ -1,6 +1,80 @@
+import { BsFacebook, BsInstagram, BsTiktok } from "react-icons/bs";
+
 export const footerData = [
     {
-        name: "attractions",
+        name: "activities",
+        link: "/activities",
+        submenu: [
+            {
+                name: "attractions",
+                link: "/activities/attractions"
+            },
+            {
+                name: "products",
+                link: "/activities/products"
+            },
+            {
+                name: "weddings",
+                link: "/activities/weddings"
+            },
+            {
+                name: "school bookings",
+                link: "/activities/school-bookings"
+            }
+        ]
+    },
+    {
+        name: "about us",
+        link: "/about-us",
+        submenu: [
+            {
+                name: "our farm",
+                link: "/about-us#our-farms"
+            },
+            {
+                name: "meet the farmers",
+                link: "/about-us#meet-the-farmers"
+            },
+            {
+                name: "history",
+                link: "/about-us#history"
+            },
+        ]
+    },
+    {
+        name: "contact us",
+        link: "/contact-us",
+        submenu: [
+            {
+                name: "support",
+                link: "/support"
+            },
+            {
+                name: "careers",
+                link: "/careers"
+            },
+        ]
+    },
+    {
+        name: "follow us",
+        link: "/contact-us",
+        submenu: [
+            {
+                name: "facebook",
+                link: "https://www.facebook.com/McMillanFarms.CornMaze.and.PumpkinPatch",
+                icon: <BsFacebook />
+            },
+            {
+                name: "instagram",
+                link: "https://www.instagram.com/mcmillanfarms/",
+                icon: <BsInstagram />
+            },
+            {
+                name: "tik tok",
+                link: "https://www.tiktok.com/@mcmillanfarms",
+                icon: <BsTiktok />
+            },
 
+        ]
     }
 ]
