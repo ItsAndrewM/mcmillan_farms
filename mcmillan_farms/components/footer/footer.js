09db028fbdd1c5007aadfbe6ed1aa2a306c47567
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-08-18 22:49:32 -0700

    muffins

--- mcmillan_farms/components/footer/footer.js
+++ mcmillan_farms/components/footer/footer.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./footer.module.css";
 import utilStyles from "../../styles/utils.module.css";
 import Link from "next/link";
-import layoutStyles from "../layout/layout.module.css"
+import layoutStyles from "../layout/layout.module.css";
 import logo from "../../public/images/logos/logo_text_right.png";
 import { footerData } from "@/data/footerData";
 
@@ -25,25 +25,65 @@ const Footer = () => {
             {footerData.map((menuItem, index) => {
               return (
                 <ul className={styles.column} key={index}>
-                  <li><h3><Link href={menuItem.link} className={layoutStyles.link}>{menuItem.name}</Link></h3></li>
+                  <li>
+                    <h3>
+                      <Link href={menuItem.link} className={layoutStyles.link}>
+                        {menuItem.name}
+                      </Link>
+                    </h3>
+                  </li>
                   <ul>
                     {menuItem.submenu.map((subMenuItem, index) => {
                       return (
-                        <li key={index}>{subMenuItem.icon && subMenuItem.icon}<Link className={layoutStyles.link} href={subMenuItem.link}>{subMenuItem.name}</Link></li>
-                      )
+                        <li key={index}>
+                          {subMenuItem.icon && subMenuItem.icon}
+                          <Link
+                            className={layoutStyles.link}
+                            href={subMenuItem.link}
+                          >
+                            {subMenuItem.name}
+                          </Link>
+                        </li>
+                      );
                     })}
                   </ul>
                 </ul>
-              )
+              );
             })}
           </div>
         </div>
         <div className={styles.container}>
-          <div><p>© McMillan Farms 2023 • Site by MuffinWare</p></div>
+          <div>
+            <p>
+              © McMillan Farms 2023 • Site by{" "}
+              <span>
+                <Link
+                  href={"https://muffinware.vercel.app/"}
+                  className={layoutStyles.link}
+                >
+                  MuffinWare
+                </Link>
+              </span>
+            </p>
+          </div>
           <div className={styles.legal}>
-            <span><Link className={layoutStyles.link} href={"/legal/privacy-policy"}>Privacy Policy</Link></span>
+            <span>
+              <Link
+                className={layoutStyles.link}
+                href={"/legal/privacy-policy"}
+              >
+                Privacy Policy
+              </Link>
+            </span>
             <span>|</span>
-            <span><Link className={layoutStyles.link} href={"/legal/terms-and-conditions"}>Terms and Conditions</Link></span>
+            <span>
+              <Link
+                className={layoutStyles.link}
+                href={"/legal/terms-and-conditions"}
+              >
+                Terms and Conditions
+              </Link>
+            </span>
           </div>
         </div>
       </div>
