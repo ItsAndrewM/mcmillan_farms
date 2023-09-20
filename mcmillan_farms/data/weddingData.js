403411a72fc42318ab51a8c831249dc76617a0e6
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-09-20 14:42:59 -0700

    removed oak bay placeholders

--- mcmillan_farms/data/weddingData.js
+++ mcmillan_farms/data/weddingData.js
@@ -2,26 +2,36 @@ export const weddingData = [
   {
     summary: "Access to all Venue Spaces",
     content:
-      "The Full Wedding Package includes use of Seaside Gardens for your ceremony and use of the David Foster Foundation Theatre for your reception, as well as all of the venue space inclusions (such as complimentary ceremony chairs, ice water stations, all required guest tables and chairs, etc)",
+      "The Full Wedding Package includes hilltop views, open fields, iconic trees, and farm animals. You can choose from a variety of locations on the farm for your ceremony and reception, the hay bale area, or the willow tree.",
   },
   {
-    summary: "Hotel Stay",
+    summary: "The Willow Tree",
     content:
-      "The wedding couple receives two nights in a romantic Ocean View Guest Room. After a long day of love and celebration, we offer breakfast in bed for the newlyweds the morning after, as well as complimentary late checkout of 1:00 pm.",
+      "If you are looking for a romantic and rustic setting for your wedding, the Willow Tree might be the perfect choice. This large willow tree is located in open pastures, surrounded by nature and tranquility. You can exchange your vows under the shade of its branches, while enjoying the breeze and the sound of birds. The Willow Tree can accommodate up to 200 guests, and offers a beautiful backdrop for your wedding photos.",
   },
   {
-    summary: "Ceremony & Cocktail Reception",
+    summary: "The Sidehill",
     content:
-      "Exclusive use of the Gardens, as well as access for the rehearsal, white ceremony chairs, a signing table and linens, access to electrical outlets, cocktail reception tables and complimentary ice water stations.",
+      "For a scenic and serene wedding, the Sidehill is a great option. This location offers a stunning view of the forest, pumpkin fields and pastures, creating a contrast of colors and textures. You can have your ceremony on a grassy slope, overlooking the landscape, or on a flat area near the trees. The Sidehill can host up to 150 guests, and provides a natural and peaceful atmosphere for your wedding.",
   },
   {
-    summary: "Reception",
+    summary: "Tents",
     content:
-      "Access to use of the David Foster Foundation Theatre from 8:00 am to 1:00 am, all required guest tables and chairs, table linens, complete place settings and glassware, head table, room set-up and tear down, and more. Please refer to our Wedding Brochure for full details.",
+      "If you want to have your wedding in any weather, tents are a cozy and comfortable option. You can choose from different sizes and styles of tents, depending on your needs and preferences. Tents can be set up in any location on the farm, giving you flexibility and convenience. Inquire below to confirm tent availability.",
   },
   {
-    summary: "Exclusive Room Rates",
+    summary: "Benches and Picnic Tables",
     content:
-      "We offer Block and Rate-Only booking options for you and your wedding guests.",
+      "If you are looking for a casual and fun way to celebrate your wedding, benches and picnic tables are a good choice. You can have your ceremony and reception in one place, using the benches and picnic tables provided by the farm. You can decorate them with tablecloths, flowers, candles, or anything else you like.",
+  },
+  {
+    summary: "Additional Information",
+    content:
+      "For any of these locations, you can enjoy the following benefits: Use of site for up to 2 ½ hours on wedding day, Up to 1 hour rehearsal time available, Hayrides can be offered to wedding site for up to 50 people. Additional cost for 50+, Washroom set-up near wedding site, Parking in pastures and/or along Berard Rd, Great wedding photo locations available after the wedding. Depending on the season, beautiful apricot or prune tree orchards, pumpkin patch, hayfields with bales, old barns, beautiful cottonwood trees, open pastures with horses and more..",
+  },
+  {
+    summary: "Cost",
+    content:
+      "The cost for any of these locations is $1,200 + GST. A non-refundable deposit of $500 is required to hold your reservation. For more details or to schedule a viewing, please contact us at information@mcmillanfarms.ca. We look forward to hosting your special day at McMillan Farms!",
   },
 ];
